acb51e74216ff81ac7a50e3dce427526173022a3
richardjim <richardiyevhobu@gmail.com>
2022-11-28 17:02:42 +0100

    init

---
+++ resources/js/components/employee/EmployeeForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Employee Form</div>
+
+                    <div class="card-body">
+                        HELLO
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
---
+++ resources/views/employee/form.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="app">
+    <employee-form></employee-form>
+</div>
+@endsection
